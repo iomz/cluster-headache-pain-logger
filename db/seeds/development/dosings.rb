0aa49313890d80f4d28571d16668761bdd37c921
iomz <iomz@sfc.wide.ad.jp>
2013-07-14 17:11:52 +0900

    Highcharts added and clean up css

--- db/seeds/development/dosings.rb
+++ db/seeds/development/dosings.rb
@@ -1,10 +1,10 @@
-# Insert sample dosings for attack ID 0:9
+# Insert sample dosings for attack ID 0:19
 
 drug_id = 0
 
-0.upto(29) do |n|
+0.upto(59) do |n|
     dosed_t = DateTime.parse(n.days.from_now.to_time.to_s)
-    user_id = n/10 + 1
+    user_id = n/20 + 1
     attack_id = n + 1
     Dosing.create(:user_id => user_id, :attack_id => attack_id, :drug_id => drug_id, :dosed_at => dosed_t)
 end
--- db/seeds/development/stocks.rb
+++ db/seeds/development/stocks.rb
@@ -2,3 +2,29 @@
 1.upto(3) do |user_id|
   Stock.create(:user_id => user_id, :drug_id => 0, :quantity_available => 10)
 end
+
+for i in 1..9
+  1.upto(3) do |user_id|
+    Stock.create(:user_id => user_id, :drug_id => 0, :quantity_available => 10-i)
+  end
+end
+
+1.upto(3) do |user_id|
+  Stock.create(:user_id => user_id, :drug_id => 0, :quantity_available => 11)
+end
+
+for i in 1..9
+  1.upto(3) do |user_id|
+    Stock.create(:user_id => user_id, :drug_id => 0, :quantity_available => 11-i)
+  end
+end
+
+1.upto(3) do |user_id|
+  Stock.create(:user_id => user_id, :drug_id => 0, :quantity_available => 12)
+end
+
+for i in 1..2
+  1.upto(3) do |user_id|
+    Stock.create(:user_id => user_id, :drug_id => 0, :quantity_available => 12-i)
+  end
+end
